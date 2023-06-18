347c714827ae970fd3ae25b00fcba6e74aa4ca3a
Richard T. Jones <richard.t.jones@uconn.edu>
2023-06-18 05:12:16 -0400

    * add xrootd to library path [rtj]

--- scripts/setup_root.sh
+++ scripts/setup_root.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export ROOTSYS=/cvmfs/oasis.opensciencegrid.org/gluex/root-6.22.06/x86_64
+export XROOTDLIB=/cvmfs/oasis.opensciencegrid.org/gluex/xrootd/3.3.2/lib64
 if [[ -d $ROOTSYS ]]; then
     export PATH=$ROOTSYS/bin:$PATH
-    export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$ROOTSYS/lib:$XROOTDLIB:$LD_LIBRARY_PATH
     export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
 fi
