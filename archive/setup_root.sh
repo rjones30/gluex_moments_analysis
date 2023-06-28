9943b8ce34ca7b9f6b831943bee8e59d2a77d52b
Richard T. Jones <richard.t.jones@uconn.edu>
2023-06-28 13:39:13 -0400

    * don't include the local archive dir in git [rtj]

--- archive/setup_root.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ROOTSYS=/cvmfs/oasis.opensciencegrid.org/gluex/root-6.22.06/x86_64
-if [[ -d $ROOTSYS ]]; then
-    export PATH=$ROOTSYS/bin:$PATH
-    export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
-    export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
-fi
