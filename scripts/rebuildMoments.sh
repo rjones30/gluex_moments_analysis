cf24876b4fee84752a83d68757d9cfe1a4d6c077
rtj02001 <rtj02001@cn410.storrs.hpc.uconn.edu>
2023-06-28 13:35:17 -0400

    * introduce new helper script to run analyzeMomentsMatrix.hinhout   over the full kinematic range of the etapi0p reaction [rtj]

--- scripts/rebuildMoments.sh
+++ scripts/rebuildMoments.sh
@@ -10,6 +10,8 @@
 # author: richard.t.jones at uconn.edu
 # version: june 27, 2023
 
+maxprocesses=1
+
 function usage {
     echo "Usage: (inside gluex container) [/srv]> rebuildMoments.sh [<mX_min>,<Mx_max> <|t_min|,|t_max|>]"
     echo " where kinematic bounds on the resonance invariant mass <mX_min>,<mX_max> and Mandelstam t,"
@@ -24,10 +26,10 @@ function clean_exit {
     exit $retcode
 }
 
-if [ $# = 3 ]; then
+if [ $# = 2 ]; then
     mspec=$1
     tspec=$2
-elif [ $# != 1 ]; then
+elif [ $# != 0 ]; then
     usage
 elif [ $(pwd) != "/srv" ]; then
     usage
@@ -39,14 +41,15 @@ source scripts/setup_scripts.sh
 function build_moments {
     mspec=$1
     tspec=$2
-    workdir="buildMoments_${mspec}_${tspec}"
-    rm -rf $workdir || clean_exit "unable to create workdir $workdir"
-    cp src C_buildMomentsMatrix* $workdir || clean_exit "unable to populate workdir $workdir"
+    workdir="rebuildMoments_${mspec}_${tspec}"
+    rm -rf $workdir || clean_exit "unable to clear space for workdir $workdir"
+    mkdir $workdir || clean_exit "unable to create workdir $workdir"
+    cp src/C_buildMomentsMatrix* $workdir || clean_exit "unable to populate workdir $workdir"
     cd $workdir || clean_exit "unable to descend into workdir $workdir"
     cat >rebuild.py <<EOI
 import sys
 import analyzeMomentsMatrix as ana
-h=ana.hinhout(massEtaPi0_limits=$mspec, abst_limits=$tspec, model=1)
+h=ana.hinhout(massEtaPi0_limits=($mspec), abst_limits=($tspec), model=1)
 sys.exit(0)
 EOI
     python3 rebuild.py || clean_exit "script rebuild.py failed"
@@ -54,11 +57,16 @@ EOI
 
 pidlist=""
 if [ "$mspec" != "" ]; then
-    build_moments $mspec $tspec >build_moments_${mspec}_${tspec}.log 2>&1 & pidlist="$pidlist $$"
+    build_moments $mspec $tspec >rebuildMoments_${mspec}_${tspec}.log 2>&1 & pidlist="$pidlist $!"
 else
     for tspec in 0.0,0.3 0.3,0.6 0.6,1.2; do
         for mspec in 0.9,1.2 1.2,1.5 1.5,1.8 1.8,2.1; do
-            build_moments $mspec $tspec >build_moments_${mspec}_${tspec}.log 2>&1 & pidlist="$pidlist $$"
+            build_moments $mspec $tspec >rebuildMoments_${mspec}_${tspec}.log 2>&1 & pidlist="$pidlist $!"
+	    if [ $(echo $pidlist | wc -w) -ge $maxprocesses ]; then
+		set $pidlist
+		wait $1 || clean_exit "child proces $pid crashed"
+                pidlist="$2 $3 $4 $5 $6 $7 $8 $9"
+            fi
         done
     done
 fi
