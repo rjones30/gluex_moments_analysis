d310cb20eccc94fd533bae4734f8e555c6744e52
Richard T. Jones <richard.t.jones@uconn.edu>
2023-07-08 16:01:43 -0400

    * still trying to figure out how package_dir and package_data work [rtj]

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ from setuptools import setup
 # the name, author and contact, and description.
 setup(
     name='gluex_moments_analysis',
-    version='0.1.1',
+    version='0.1.2',
     author='Richard Jones',
     author_email='richard.t.jones@uconn.edu',
     description='toolkit for implementation of support-vector moments analysis',
     license='GPL',
     url='https://github.com/rjones30/gluex_moments_analysis',
     packages=['gluex_moments_analysis'],
+    package_dir={"src": "src"}
     package_data={"src": ["*.C", "*.h", "*.c", "Makefile"]},
     install_requires=[
             'numpy',
