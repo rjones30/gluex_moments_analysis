0a8c981559aede9065c9b2b490f8957cfd71b085
Richard T. Jones <richard.t.jones@uconn.edu>
2023-07-10 08:16:05 -0400

    * advance the version (woops) [rtj]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 # the name, author and contact, and description.
 setup(
     name='gluex_moments_analysis',
-    version='0.1.16',
+    version='0.1.17',
     author='Richard Jones',
     author_email='richard.t.jones@uconn.edu',
     description='toolkit for implementation of support-vector moments analysis',
