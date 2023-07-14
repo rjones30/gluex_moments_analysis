c2513b9bebf18c96649f995fd76da2244062a20c
rtj02001 <rtj02001@cn410.storrs.hpc.uconn.edu>
2023-07-14 11:29:06 -0400

    * move the ROOT macro fill_M_histograms.C to the package directory [rtj]

--- gluex_moments_analysis/src/fill_M_histograms.C
+++ gluex_moments_analysis/src/fill_M_histograms.C
@@ -1,7 +1,7 @@
 #include <TH1D.h>
 #include <iostream>
 
-void fill_M_histograms(const double *M, const double *Mvar, int m, int mdim, TH1D *hMabovediag, TH1D *hMbelowdiag, TH1D *hEabovediag, TH1D *hEbelowdiag)
+void fill_M_histograms(const double *M, int m, int mdim, TH1D *hMabovediag, TH1D *hMbelowdiag)
 {
   int mm(m * mdim + m);
   int mn(m * mdim);
@@ -9,12 +9,10 @@ void fill_M_histograms(const double *M, const double *Mvar, int m, int mdim, TH1
   for (n=0; n < m; ++n, ++mn) {
     int nn(n * mdim + n);
     hMbelowdiag->Fill(M[mn] / sqrt(M[mm] * M[nn]));
-    hEbelowdiag->Fill(sqrt(Mvar[mn] / (M[mm] * M[nn])));
   }
   ++mn;
   for (n=m+1; n < mdim; ++n, ++mn) {
     int nn(n * mdim + n);
     hMabovediag->Fill(M[mn] / sqrt(M[mm] * M[nn]));
-    hEabovediag->Fill(sqrt(Mvar[mn] / (M[mm] * M[nn])));
   }
 }
